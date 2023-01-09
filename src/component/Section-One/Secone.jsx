62f615e05780a7dc8dd8d311cb43236df2d0cb70
unknown <bilalahmed19971807@gmail.com>
2023-01-09 21:14:01 +0500

    Fouth Commit

--- src/component/Section-One/Secone.jsx
+++ src/component/Section-One/Secone.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../Section-One/Secone.css";
 import laptop from "../Section-One/assets/Laptop.png";
-import arrow from "../Section-One/assets/arrow.png"
+import arrow from "../Section-One/assets/arrow.png";
 function Secone() {
   return (
     <div>
       <div className="container-fluid my-5 mc-container">
-        <div className="row my-4">
+        <div className="row my-4 mc-main-row">
+          <a className="sticky-btn" href="/">
+            CONTACT US !
+          </a>
+
           <div className="col-8 mc-col-one">
             <h1 className="mc-main-heading mc-content">
               Make beautiful, <br />
@@ -25,26 +29,29 @@ function Secone() {
               <li>Conceptual Ecommerce Designs</li>
               <li>Easy Payment Module Integrations</li>
             </ul>
-            <br/>
+            <br />
             <a href="/">
-            <div className="arrow-img rounded-circle mc-content">
-           <img src={arrow} alt="" className="arrow-img" />
-            </div>
-            <span className="mc-content text-get">GET A QUOTE</span>
+              <div className="arrow-img rounded-circle mc-content">
+                <img src={arrow} alt="" className="arrow-img" />
+              </div>
+              <span className="mc-content text-get">GET A QUOTE</span>
             </a>
-            <span className="mc-content text-start">STARTTING FROM <span className="price">$599</span></span>
+            <span className="mc-content text-start">
+              STARTING FROM &nbsp;<span className="price">$599</span>.99
+            </span>
           </div>
 
           <div className="col-4 mc-laptop-col">
-        
             <div className="mc-laptop-sub-container">
-            <img src={laptop} alt="" className="laptop-img" />
+              <img src={laptop} alt="" className="laptop-img" />
             </div>
-
-            
-
           </div>
+          <div className="col-8">
+            <span>FACEBOOK</span> &nbsp;
+            <span>TWITTER</span> &nbsp;
+            <span>INSTAGRAM</span> 
           </div>
+        </div>
       </div>
     </div>
   );
